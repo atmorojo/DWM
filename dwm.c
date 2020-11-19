94130c0abbd4c6bcb4ef64aebaff372c0d301252
Risyad Rais <satmo54@gmail.com>
2020-11-19 10:16:47 +0700

    Comment

--- dwm.c
+++ dwm.c
@@ -764,6 +764,7 @@ drawbar(Monitor *m)
 	drw_setscheme(drw, scheme[SchemeSel]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0);
 
+	// Window name
 	if ((w = m->ww - tw - x) > bh) {
 		if (m->sel) {
 			drw_setscheme(drw, scheme[SchemeNorm]);
